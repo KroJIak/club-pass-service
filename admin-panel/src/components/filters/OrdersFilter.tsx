60f4a065838c06f254559a29cfe385b3197429c9
KroJIak <rastoandrei@gmail.com>
2025-12-24 12:23:31 +0400

    refactor: simplify OrdersFilter by removing promocode section

--- admin-panel/src/components/filters/OrdersFilter.tsx
+++ admin-panel/src/components/filters/OrdersFilter.tsx
@@ -1,21 +1,11 @@
-import { useState, useEffect } from 'react'
-import {
-  Box,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  Typography,
-} from '@mui/material'
-import { FilterSection } from './FilterPanel'
+import { Box } from '@mui/material'
 
 export interface OrdersFilterState {
   search: string
-  hasPromocode: 'all' | 'yes' | 'no'
 }
 
 const DEFAULT_FILTER_STATE: OrdersFilterState = {
   search: '',
-  hasPromocode: 'all',
 }
 
 interface OrdersFilterProps {
@@ -24,46 +14,8 @@ interface OrdersFilterProps {
 }
 
 const OrdersFilter = ({ filterState, onFilterChange }: OrdersFilterProps) => {
-  const [localFilter, setLocalFilter] = useState<OrdersFilterState>(filterState)
-
-  useEffect(() => {
-    setLocalFilter(filterState)
-  }, [filterState])
-
-  const handleFilterChange = (updates: Partial<OrdersFilterState>) => {
-    const newFilter = { ...localFilter, ...updates }
-    setLocalFilter(newFilter)
-    onFilterChange(newFilter)
-  }
-
-  return (
-    <Box>
-      <FilterSection title="Promocode">
-        <RadioGroup
-          value={localFilter.hasPromocode}
-          onChange={(e) => handleFilterChange({ hasPromocode: e.target.value as 'all' | 'yes' | 'no' })}
-        >
-          <FormControlLabel 
-            value="all" 
-            control={<Radio size="small" sx={{ '& .MuiSvgIcon-root': { fontSize: '1rem' } }} />} 
-            label={<Typography sx={{ fontSize: '0.84rem' }}>All</Typography>} 
-          />
-          <FormControlLabel 
-            value="yes" 
-            control={<Radio size="small" sx={{ '& .MuiSvgIcon-root': { fontSize: '1rem' } }} />} 
-            label={<Typography sx={{ fontSize: '0.84rem' }}>Has promocode</Typography>} 
-          />
-          <FormControlLabel 
-            value="no" 
-            control={<Radio size="small" sx={{ '& .MuiSvgIcon-root': { fontSize: '1rem' } }} />} 
-            label={<Typography sx={{ fontSize: '0.84rem' }}>No promocode</Typography>} 
-          />
-        </RadioGroup>
-      </FilterSection>
-    </Box>
-  )
+  return <Box />
 }
 
 export default OrdersFilter
 export { DEFAULT_FILTER_STATE }
-
